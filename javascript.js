13e15f51e5c0cd231994c58c08e52b3c83a1e92d
Vlad David <vlad.david@brunomag.ro>
2016-09-27 20:38:03 +0300

    27sep2016 fix + tranzitii
    scrol, insert smiley, transitii

--- javascript.js
+++ javascript.js
@@ -7,15 +7,15 @@ function iesSmile() {
 	
 	
 	
-	document.getElementById('emoji-ul').style.visibility='visible';
-	document.getElementById('emoji-ul').style.transition='all 0.1s ease-in';
+	document.getElementById('emoji-ul').style.opacity='1';
+	document.getElementById('emoji-ul').style.transition='all 0.3s ease-in';
 
 	ifClicked++;
 	} else {
 		
 		
-		document.getElementById('emoji-ul').style.visibility='hidden';
-		document.getElementById('emoji-ul').style.transition='all 0.1s ease-out';
+		document.getElementById('emoji-ul').style.opacity='0';
+		document.getElementById('emoji-ul').style.transition='all 0.2s ease-out';
 		
 		ifClicked++;
 	}
@@ -27,15 +27,35 @@ function schimb() {
 
 		document.getElementById('sectiune-participanti').style.width='5%';
 		document.getElementById('fereastra-chat').style.width='83.5%';
+
+		document.getElementById('participanti-wapper').style.opacity='0';
 		document.getElementById('participanti-wapper').style.display='none';
-		document.getElementById('sectiune-participanti-redus').style.display='initial';
+
+		document.getElementById('sectiune-participanti-redus').style.opacity='1';
+
+		document.getElementById('sectiune-participanti').style.background='transparent';
+		document.getElementById('sectiune-participanti').style["box-shadow"]='none';
+
+		document.getElementById('titlu-participanti').style.opacity='0';
+		document.getElementById('participanti-icon').style.opacity='0';
+
 		ifSchimb++;
 	} else {
 	
 		document.getElementById('sectiune-participanti').style.width='25%';
 		document.getElementById('fereastra-chat').style.width='63.5%';
+
+		document.getElementById('participanti-wapper').style.opacity='1';
 		document.getElementById('participanti-wapper').style.display='initial';
-		document.getElementById('sectiune-participanti-redus').style.display='none';
+
+		document.getElementById('sectiune-participanti-redus').style.opacity='0';
+
+		document.getElementById('sectiune-participanti').style.background='#fff';
+		document.getElementById('sectiune-participanti').style["box-shadow"]='2px 2px 10px #aaa';
+
+		document.getElementById('titlu-participanti').style.opacity='1';
+		document.getElementById('participanti-icon').style.opacity='1';
+
 		ifSchimb++;
 	}
 
@@ -45,15 +65,39 @@ function schimb() {
 function sendtext () {
 
 	if (document.getElementById("textar").value!=="" ) {
-		var y = document.getElementById("textar").value;
-		var z = "<strong>Me</strong>: " + y + "<br>" ;
-		var x = document.getElementById("fereastra-chat").innerHTML;
-	    document.getElementById("fereastra-chat").innerHTML = x+ z;
-	    document.getElementById("textar").value="";
+		
+		
+			
+		
+			var y = document.getElementById("textar").value;
+			console.log(y.search("-smile-"));
+			if (cautaSmile()){
+				var a = y.substr(0, y.search("-smile-"));
+				var b = y.substr(y.search("-smile-")+7, length.y);
+				console.log(a);
+				var z = "<strong>Me</strong>: " + a + "<img src='img/smile.png' style='width:20px; height:20px;'>" + b + "<br>" ;
+				console.log(z);
+				var x = document.getElementById("fereastra-chat").innerHTML;
+			    document.getElementById("fereastra-chat").innerHTML = x+ z;
+			    document.getElementById("textar").value="";
+			} else {
+				var z = "<strong>Me</strong>: " + y + "<br>" ;
+				var x = document.getElementById("fereastra-chat").innerHTML;
+			    document.getElementById("fereastra-chat").innerHTML = x+ z;
+			    document.getElementById("textar").value="";
+		    }
+		
 	}
+}
 
-	
-
+function cautaSmile() {
+	var a=document.getElementById("textar").value;
+	var b = document.getElementById("textar").value.search("-smile-");
+	if (b==-1){
+		return false;
+	} else {
+		return true;
+	}
 }
 
 function pressEnt (e) {
@@ -64,4 +108,11 @@ function pressEnt (e) {
 			
 		}
 	}
+}
+
+function apareEmoji() {
+		var y = document.getElementById("textar").value;
+		var z = y + "-smile-";
+		document.getElementById("textar").value= z;
+
 }
